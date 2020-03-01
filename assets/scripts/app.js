0eafa7e55bc04ccc224150c798f0dc1fde90378e
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-01 20:01:53 +0100

    add another marker with info window and the beachflag icon

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,4 @@
+let map;
 function initMap() {
   // zoom level
   //  1 : World
@@ -13,11 +14,36 @@ function initMap() {
     center: Linz
   };
   // new map
-  const map = new google.maps.Map(document.getElementById('map'), options);
+  map = new google.maps.Map(document.getElementById('map'), options);
   // set marker
   const marker = new google.maps.Marker({
     position: Linz,
     map: map,
     title: 'state capital of Upper Austria'
+  });
+
+  addMarkerWithInfoWindow();
+}
+
+// Add marker with different icon and infoWindow
+function addMarkerWithInfoWindow() {
+  const Wels = { lat: 48.16667, lng: 14.03333 };
+  const marker = new google.maps.Marker({
+    position: Wels,
+    map: map,
+    title: 'click me for infos',
+    icon:
+      'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+  });
+
+  const infoWindow = new google.maps.InfoWindow({
+    content:
+      '<h3>Disctrict Capital of Upper Austria</h3>' +
+      '<p>Wels (German pronunciation: [vɛls]) is a city in Upper Austria, on the Traun River near Linz. ' +
+      'It is the county seat of Wels-Land, and with a population of approximately 60,000, the eighth largest city in Austria.</p>'
+  });
+
+  marker.addListener('click', function() {
+    infoWindow.open(map, marker);
   });
 }
