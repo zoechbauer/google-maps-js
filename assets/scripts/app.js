c06e8d64848e537d3e1bb8e5e952419e17346d3e
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-01 18:33:41 +0100

    display map centered at Linz, Austria and set a Marker with Info

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,3 +1,23 @@
 function initMap() {
-  alert('Test: initMap called');
+  // zoom level
+  //  1 : World
+  // 5: Landmass/continent
+  // 10: City
+  // 15: Streets
+  // 20: Buildings
+
+  // map options
+  const Linz = { lat: 48.30639, lng: 14.28611 };
+  const options = {
+    zoom: 10,
+    center: Linz
+  };
+  // new map
+  const map = new google.maps.Map(document.getElementById('map'), options);
+  // set marker
+  const marker = new google.maps.Marker({
+    position: Linz,
+    map: map,
+    title: 'state capital of Upper Austria'
+  });
 }
